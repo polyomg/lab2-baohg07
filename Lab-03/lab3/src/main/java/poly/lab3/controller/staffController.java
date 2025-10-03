a6680d8ac9fc0055e3a30fbaa0f94a66d08cfa79
baohg07 <baohg82@gmail.com>
2025-10-03 15:30:14 +0700

    add lab3 bai2

--- Lab-03/lab3/src/main/java/poly/lab3/controller/staffController.java
+++ Lab-03/lab3/src/main/java/poly/lab3/controller/staffController.java
@@ -5,6 +5,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import poly.lab3.model.staff;
 
+import java.util.List;
+
 @Controller
 public class staffController {
     @RequestMapping("/staff/detail")
@@ -18,5 +20,18 @@ public class staffController {
 
         model.addAttribute("staff", Staff);
         return "/staff-detail";
+    }
+
+    @RequestMapping("/staff/list")
+    public String list(Model model) {
+        List<staff> list = List.of(
+                staff.builder().id("user1@gmail.com").fullname("nguyễn văn user1").level(0).build(),
+                staff.builder().id("user2@gmail.com").fullname("nguyễn văn user2").level(1).build(),
+                staff.builder().id("user3@gmail.com").fullname("nguyễn văn user3").level(2).build(),
+                staff.builder().id("user4@gmail.com").fullname("nguyễn văn user4").level(2).build(),
+                staff.builder().id("user5@gmail.com").fullname("nguyễn văn user5").level(1).build(),
+                staff.builder().id("user6@gmail.com").fullname("nguyễn văn user6").level(0).build());
+        model.addAttribute("list", list);
+        return "/staff-list";
     }
 }
